8a290cebb674916b7dbb452ac61f213464adc495
Tianhaosheng <961791615@qq,com>
2020-10-21 16:43:34 +0800

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world -- git-分支--test");
+        System.out.println("hello world -- git-分支-");
     }
 }
